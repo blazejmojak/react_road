35f99892f8afa5bbc1ff5afb16c98a566c907094
moyaky <blazejmojak@gmail.com>
2023-10-31 15:40:38 +0100

    useCallback and fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { useEffect, ChangeEvent, useReducer } from 'react'
+import { useEffect, ChangeEvent, useReducer, useCallback } from 'react'
 import './App.css'
 import { useSearchField } from './customHooks/useSearchField';
 import CustomInput from './components/CustomInput';
 import StoriesList from './components/StoriesList';
 import { storiesReducer } from './reducers/storiesReducer';
+
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 export type Story = {
   title: string;
@@ -22,61 +24,36 @@ export type StoriesState = {
 
 export type TonRemoveItem = (item: Story) => void;
 
-
-const initialStories: Story[] = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: 'TypeScript',
-    url: 'https://typescript.pro/',
-    author: 'Janusz Kowalski',
-    num_comments: 4,
-    points: 9,
-    objectID: 11,
-  },
-
-];
-
-const getAsyncStories = (): Promise<{ data: { stories: Story[] } }> =>
-  new Promise((resolve) =>
-    setTimeout(() => {
-      resolve({ data: { stories: initialStories } })
-    }, 3000)
-  )
+// const getAsyncStories = (): Promise<{ data: { stories: Story[] } }> =>
+//   new Promise((resolve) =>
+//     setTimeout(() => {
+//       resolve({ data: { stories: initialStories } })
+//     }, 3000)
+//   )
 
 function App() {
   const [searchText, setsearchText] = useSearchField('searchField', '');
   // const [stories, setstories] = useState<Story[]>([]);
   const [stories, dispatchStories] = useReducer(storiesReducer, { data: [], isLoading: false, isError: false });
 
-  useEffect(() => {
+  const handleFetchStories = useCallback(() => {
+    if (!searchText) return;
+
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}${searchText}`)
+      .then((response) => response.json())
       .then((result) => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.stories,
-        });
-      })
-      .catch(() =>
-        dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
-      );
-  }, [])
+          payload: result.hits,
+        })
+      });
+  }, [searchText])
+
+  useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories])
 
   const handleRemoveStory: TonRemoveItem = (item: Story) => {
     dispatchStories({
@@ -90,9 +67,6 @@ function App() {
     setsearchText(event.target.value)
   }
 
-  const searchedStories = stories.data.filter((story) =>
-    story.title.toLowerCase().includes(searchText.toLowerCase())
-  );
 
   return (
     <>
@@ -103,7 +77,7 @@ function App() {
         <strong>Search Field value is: {searchText}</strong>
       </p>
       {stories.isError && <p>Some Data Loading Error...</p>}
-      {stories.isLoading ? 'Is Loading...' : <StoriesList stories={searchedStories} onRemoveItem={handleRemoveStory} />}
+      {stories.isLoading ? 'Is Loading...' : <StoriesList stories={stories.data} onRemoveItem={handleRemoveStory} />}
     </>
   )
 }
